e0f04a5d467d0ab7d80f38bf46c4ff49df329aaf
Nik Kov <nikkovios@gmail.com>
2017-05-23 12:15:20 +0300

    Initial Commit

---
+++ NKVPhonePicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NKVPhonePicker
+//
+//  Created by Nik Kov on 23.05.17.
+//  Copyright © 2017 nik.kov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
