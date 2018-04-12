8d52c67be789a4b7bbbce6c2e66085eabfb555e0
Nik Kov <nikkovios@gmail.com>
2018-04-13 02:21:54 +0300

    Carthage support! Changed theme to shared and added framework target.

---
+++ NKVPhonePicker/NKVPhonePicker.h
@@ -0,0 +1,19 @@
+//
+//  NKVPhonePicker.h
+//  NKVPhonePicker
+//
+//  Created by Nik Kov on 13.04.2018.
+//  Copyright © 2018 nik.kov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NKVPhonePicker.
+FOUNDATION_EXPORT double NKVPhonePickerVersionNumber;
+
+//! Project version string for NKVPhonePicker.
+FOUNDATION_EXPORT const unsigned char NKVPhonePickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NKVPhonePicker/PublicHeader.h>
+
+
